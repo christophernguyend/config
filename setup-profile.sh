fd95da2756c86c952dbdb8963cb45853a3f47166
Christopher D. Nguyen <christopher.nguyen.d@gmail.com>
2014-05-12 11:43:35 -0400

    Added executable scripts and SSH configuration file.

--- setup-profile.sh
+++ setup-profile.sh
@@ -9,4 +9,9 @@ cp .gitconfig $HOME/
 echo "Configure .gitconfig with user.name, user.email, and user.signingkey."
 cp .vimrc $HOME/
 cp .zsh $HOME/
+cp .ssh/* $HOME/.ssh/
+echo "Configure SSH config with host information."
+chmod +x bin/*
+cp bin/* $HOME/bin/
+echo "Configure executable files."
 
